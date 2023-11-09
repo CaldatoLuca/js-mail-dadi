bfb14788093ea721f09321ea12e61a1cb4db119f
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-09 17:19:07 +0100

    feat: variabili random number

---
+++ esercizio 2/js/main.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+//? variabili
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+//? stampa in console
+console.log({ userNumber });
+console.log({ computerNumber });
