52d5c9d3e3967743e0975c866cb0f2f5faca9eb6
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-09 17:25:37 +0100

    feat: condizioni e output

--- esercizio 2/js/main.js
+++ esercizio 2/js/main.js
@@ -7,3 +7,15 @@ const computerNumber = Math.floor(Math.random() * 6) + 1;
 //? stampa in console
 console.log({ userNumber });
 console.log({ computerNumber });
+
+//! condizione
+if (userNumber > computerNumber) {
+  console.log("Hai vinto!");
+  console.log(userNumber + " > " + computerNumber);
+} else if (userNumber < computerNumber) {
+  console.log("Hai perso!");
+  console.log(userNumber + " < " + computerNumber);
+} else {
+  console.log("Hai pareggiato!");
+  console.log(userNumber + " = " + computerNumber);
+}
