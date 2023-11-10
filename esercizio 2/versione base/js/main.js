6bd389f10e7127a9c0fea44d5b8702672ee105ea
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-10 10:22:57 +0100

    correzzione

--- esercizio 2/versione base/js/main.js
+++ esercizio 2/versione base/js/main.js
@@ -11,6 +11,36 @@ console.log({ computerNumber });
 //! condizione
 if (userNumber > computerNumber) {
   console.log("Hai vinto!");
+  console.log(`${userNumber} > ${computerNumber}`);
+  alert("Hai vinto!");
+  alert(
+    `Il tuo numero: ${userNumber} è maggiore di quello del computer: ${computerNumber}`
+  );
+} else if (userNumber < computerNumber) {
+  console.log("Hai perso!");
+  console.log(`${userNumber} < ${computerNumber}`);
+  alert("Hai perso!");
+  alert(
+    `Il tuo numero: ${userNumber} è minore di quello del computer: ${computerNumber}`
+  );
+} else {
+  console.log("Hai pareggiato!");
+  console.log(`${userNumber} = ${computerNumber}`);
+  alert("Hai pareggiato!");
+  alert(
+    `Il tuo numero: ${userNumber} è pari a quello del computer: ${computerNumber}`
+  );
+}
+
+/*
+!Correzzione
+`use strict`;
+
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+if (userNumber > computerNumber) {
+  console.log("Hai vinto!");
   console.log(userNumber + " > " + computerNumber);
   alert("Hai vinto!");
   alert(
@@ -40,3 +70,4 @@ if (userNumber > computerNumber) {
       computerNumber
   );
 }
+*/
